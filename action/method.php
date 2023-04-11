32bd1fd2edbfd91454a9aa6e73ed8d1ecab1f090
aditya <aditya@delimp.com>
2023-04-11 17:27:07 +0530

    Celebrity Option

--- action/method.php
+++ action/method.php
@@ -1,8 +1,11 @@
 <?php 
 function addAccountPage() {
     include plugin_dir_path(__FILE__).'../template/user/controller.php';
+    include plugin_dir_path(__FILE__).'../template/celebrity/controller.php';
     add_menu_page('Account', 'Account', 'manage_options' ,'account', 'account_cb', 'dashicons-wordpress');
     add_submenu_page("account", "Add Account", "Add Account", "manage_options", "add-account", "add_account_cb");
+    add_menu_page('Celebrities', 'Celebrities', 'manage_options' ,'celebrities', 'celebrities_cb', 'dashicons-admin-users');
+    add_submenu_page("celebrities", "Add Celebrity", "Add Celebrity", "manage_options", "add-celebrity", "add_celebrity_cb");
 }
 
 function account_cb(){
@@ -24,6 +27,30 @@ function add_account_cb(){
         $value = getDetail($id);
     }
     include plugin_dir_path(__FILE__).'../template/user/add_user.php';
+    $html .= ob_get_contents();
+    ob_end_clean();
+    echo $html;
+}
+
+function celebrities_cb(){
+    sbs_author_blog_script();
+    $html = '';
+    ob_start();
+    include plugin_dir_path(__FILE__).'../template/celebrity/list_celebrity.php';
+    $html .= ob_get_contents();
+    ob_end_clean();
+    echo $html;
+}
+
+function add_celebrity_cb(){
+    sbs_author_blog_script();
+    $html = '';
+    ob_start();
+    $id=(isset($_GET['id']))?$_GET['id']:0;
+    if($id){
+        $value = Celebrity\getDetail($id);
+    }
+    include plugin_dir_path(__FILE__).'../template/celebrity/add_celebrity.php';
     $html .= ob_get_contents();
     ob_end_clean();
     echo $html;
---
+++ template/celebrity/controller.php
@@ -0,0 +1,49 @@
+<?php 
+namespace Celebrity;
+
+ if (isset($_POST['insert'])) {
+    global $wpdb;
+    $default =[
+        'celebrity_name'=>'',
+        'celebrity_imdb_id'=>'',
+        'celebrity_birth_date'=>'',
+        'celebrity_birth_place'=>'',
+        'celebrity_bio'=>'',
+        'celebrity_images'=>'',
+        'celebrity_image_galleries'=>'',
+        'celebrity_videos'=>'',
+        'celebrity_embeds'=>'',
+        'celebrity_external_media'=>'',
+        'celebrity_filmography'=>''
+    ];
+    $insert = shortcode_atts($default,$_POST);
+    $wpdb->insert('wp_2_bcc_celebrities_data',$insert);
+    wp_redirect('?page=celebrities');
+  }
+  if (isset($_POST['update'])) {
+    global $wpdb;
+    $default =[
+      'celebrity_name'=>'',
+      'celebrity_imdb_id'=>'',
+      'celebrity_birth_date'=>'',
+      'celebrity_birth_place'=>'',
+      'celebrity_bio'=>'',
+      'celebrity_images'=>'',
+      'celebrity_image_galleries'=>'',
+      'celebrity_videos'=>'',
+      'celebrity_embeds'=>'',
+      'celebrity_external_media'=>'',
+      'celebrity_filmography'=>''
+    ];
+    $update = shortcode_atts($default,$_POST);
+    $wpdb->update('wp_2_bcc_celebrities_data',$update,['ID'=>$_POST['row_id']]);
+    wp_redirect('?page=celebrities');
+  }
+
+  function getDetail($id){
+    global $wpdb;
+    $query =  "SELECT * FROM `wp_2_bcc_celebrities_data` WHERE ID={$id}";
+    $data = $wpdb->get_row($query);
+    return $data;
+  }
+?>
